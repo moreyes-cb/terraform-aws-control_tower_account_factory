d45b7e55b8c62cfe65f20f30f821dd032782ad0d
moreyes-cb <139962169+moreyes-cb@users.noreply.github.com>
2023-07-25 22:46:27 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,13 +4,6 @@
 
 terraform {
   required_version = ">= 1.3"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
 }
 
 module "packaging" {
