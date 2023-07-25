660c03e24089791b69c511325fbfff6ad6d0cd38
moreyes-cb <139962169+moreyes-cb@users.noreply.github.com>
2023-07-25 22:24:38 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,6 @@
 # Copyright Amazon.com, Inc. or its affiliates. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 #
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = "eu-west-2"
-}
 
 module "packaging" {
   source = "./modules/aft-archives"
